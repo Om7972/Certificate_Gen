370358e2a5c4695a17c33b389ac3df5c3ffdd396
Om7972 <odhumkekar@gmail.com>
2026-06-17 22:05:01 +0530

    feat: implement authentication logic and API environment configuration

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000'
-    : 'https://dbms-sce-backend.onrender.com'; // Production Backend URL
+    : 'https://certificategen-3f56.onrender.com/'; // Production Backend URL
 const API_URL = `${API_BASE_URL}/api/auth`;
 window.API_BASE_URL = API_BASE_URL;
 
